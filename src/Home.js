165992e2bda2ada50743ee33594c7d77f29502ed
douglasfrancis <dougiefrancis@gmail.com>
2022-07-15 06:35:26 +0100

    Remove slider

--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,11 @@ import React, {useEffect, useRef, useState} from 'react'
 import image1 from './wedding1.jpg'
 import image2 from './wedding3.jpg'
 import './Home.css'
-import BackgroundSlider from 'react-background-slider'
 
 export default function Home() {
  
   return (
-    <section id='home'>
-        <BackgroundSlider images={[image1, image2]} duration={7} transition={2} />
+    <section id='home' style={{backgroundImage: `url(${image1})`, height: '100vh', backgroundPosition: 'center', backgroundSize: 'cover'}}>
    
     </section>
   )
